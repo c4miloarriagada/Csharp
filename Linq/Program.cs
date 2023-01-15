1a3cb786a72b1dafc86a0990cb2aaf7fce953cdf
c4miloarriagada <camilo.arriagadav@gmail.com>
2023-01-15 13:13:40 -0300

    Linq

---
+++ Linq/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Beer> beers = new List<Beer>()
+            {
+                new Beer()
+                {
+                    Name="Corona", Country= "Mexico"
+                },
+                new Beer()
+                {
+                    Name = "Delirium", Country = "Belgica"
+                },
+                new Beer()
+                {
+                    Name="Erdinger", Country="Alemania"
+                }
+            };
+            foreach(var beer in beers)
+                Console.WriteLine(beer);
+
+            Console.WriteLine("---------------------------");
+
+            //SELECT
+
+            var beersName = from b in beers
+                            select new
+                            {
+                                Name = b.Name,
+                                Letters = b.Name.Length
+                            };
+
+            foreach(var beer in beersName)
+                Console.WriteLine($"{beer.Name} {beer.Letters}");
+
+
+            Console.WriteLine("-------------------------------");
+            var beerNameReal = from b in beersName
+                               select new
+                               {
+                                   Name = b.Name
+                               };
+            foreach(var beer in beerNameReal)
+                Console.WriteLine($"{beer.Name}");
+            Console.WriteLine("------------------------------");
+
+            var beersMexico = from b in beers
+                              where b.Country == "Mexico"
+                              || b.Country == "Alemania"
+                              select b;
+            foreach(var beer in beersMexico)
+                Console.WriteLine(beer);
+            Console.WriteLine("------------------------------");
+
+            var orderedBeers = from b in beers
+                               orderby b.Country descending
+                               select b ;
+
+            foreach(var beer in orderedBeers)
+                Console.WriteLine(beer);
+        }
+    }
+
+    public class Beer
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+
+        public override string ToString()
+        {
+            return $"Nombre: {Name} Pais : {Country}";
+        }
+    }
+
+}
---
+++ Linq_Join/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Linq;
+
+
+namespace Linq_Join
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var beers = new List<Beer>()
+            {
+                new Beer()
+                {
+                    Name= "Corona", Country = "Mexico"
+                },
+                 new Beer()
+                {
+                    Name = "Delirium", Country = "Belgica"
+                },
+                new Beer()
+                {
+                    Name="Erdinger", Country="Alemania"
+                }
+            };
+
+            var countries = new List<Country>()
+            {
+                new Country()
+                {
+                    Name = "Mexico",
+                    Continent = "America"
+                },
+                new Country()
+                {
+                    Name = "Alemania",
+                    Continent = "Europe"
+                },
+                new Country()
+                {
+                    Name = "Belgica",
+                    Continent = "Europe"
+                }
+            };
+
+            var beersWithContinent = from beer in beers
+                                     join country in countries
+                                     on beer.Country equals country.Name
+                                     select new
+                                     {
+                                         Name = beer.Name,
+                                         Country = beer.Country,
+                                         Continent = country.Continent,
+                                     };
+
+            foreach(var beer in beersWithContinent)
+                Console.WriteLine($"{beer.Name} {beer.Country} {beer.Continent}");
+
+        }
+    }
+        public class Beer
+        {
+            public string Name { get; set; }
+            public string Country { get; set; }
+        }
+
+        public class Country
+        {
+            public string Name { get; set; }
+
+            public string Continent { get; set; }
+        }
+
+
+    
+}
